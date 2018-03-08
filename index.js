f88c05c76a08c15d173e8805e607568d713adffa
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2018-03-08 14:14:53 +0800

    Response all data from mobileprovision file

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function mp(input) {
                     TeamIdentifier,
                     ExpirationDate,
                     TeamName,
+                    data:mpObj,
                     installLocation: output,
                 });
             });
